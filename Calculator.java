05cb4eba8cf342e1cbea4836d8cdc24c23a86901
JongKwang Kim <kim@jongkwang.com>
2018-10-05 14:14:30 +0900

    gpu

--- Calculator.java
+++ Calculator.java
@@ -2,4 +2,8 @@ public class Calculator {
     public static int sum(int a, int b) {
         return a + b;
     }
+    public static int minus2(int a, int b, String c) {
+        System.out.println(c);
+        return a - b;
+    }
 }
